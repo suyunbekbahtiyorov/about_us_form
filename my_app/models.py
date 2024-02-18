f41940ff7b319bc6c1271a693dd77d3c6f288725
Suyunbek Bakhtiyorov <suyunbek05.09.2005@gmail.com>
2024-02-18 10:16:07 +0500

    initial commit

--- my_app/models.py
+++ my_app/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-
-class Customer(models.Model):
-    first_name = models.CharField(max_length=100, blank=False, null=False)
-    last_name = models.CharField(max_length=100, blank=False, null=False)
-    phone_number = models.CharField(max_length=100, blank=False, null=False)
-    email = models.CharField(max_length=100, blank=False, null=False)
-    message = models.TextField(blank=False, null=False)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+# # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    company = models.CharField(max_length=200)
+    phone = models.CharField(max_length=11)
+    # area_code = models.CharField(max_length=5)
+    exist = models.CharField(max_length=3)
